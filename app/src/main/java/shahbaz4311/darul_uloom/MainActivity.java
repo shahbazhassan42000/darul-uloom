26487b748d87818774978ca5b5b1a6c6cfd5c4d7
shahbazhassan42000 <shahbazhassan42000@gmail.com>
2023-01-25 01:38:53 +0500

    added animation on buttons at main view

--- app/src/main/java/shahbaz4311/darul_uloom/MainActivity.java
+++ app/src/main/java/shahbaz4311/darul_uloom/MainActivity.java
@@ -2,12 +2,17 @@ package shahbaz4311.darul_uloom;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 import java.util.Objects;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    LinearLayout add_student_btn,records_btn,github_btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,6 +21,53 @@ public class MainActivity extends AppCompatActivity {
         //remove the title bar
         Objects.requireNonNull(getSupportActionBar()).hide();
 
+        //initialize the buttons
+        add_student_btn = findViewById(R.id.add_student_btn);
+        records_btn = findViewById(R.id.records_btn);
+        github_btn = findViewById(R.id.github_btn);
+
+
+        //add animation to the buttons
+        add_student_btn.setAlpha(0);
+        records_btn.setAlpha(0);
+        github_btn.setAlpha(0);
+        add_student_btn.setTranslationY(-50);
+        records_btn.setTranslationX(50);
+        github_btn.setTranslationY(50);
+
+        add_student_btn.animate().alpha(1).translationYBy(50).setDuration(1500).setStartDelay(400).start();
+        records_btn.animate().alpha(1).translationXBy(-50).setDuration(1500).setStartDelay(400).start();
+        github_btn.animate().alpha(1).translationYBy(-50).setDuration(1500).setStartDelay(400).start();
+
+
+
+        //set the onclick listener
+        add_student_btn.setOnClickListener(this);
+        records_btn.setOnClickListener(this);
+        github_btn.setOnClickListener(this);
+
+
+
+
+
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.add_student_btn:
+                //do something
+                break;
+            case R.id.records_btn:
+                //do something
+                break;
+            case R.id.github_btn:
+                Intent intent=new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://github.com/shahbazhassan42000/darul-uloom/commits/main"));
+                startActivity(intent);
+                break;
+        }
 
     }
 }
