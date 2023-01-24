72631d16c91448f6bda1068ae426a00055a7d89a
shahbazhassan42000 <shahbazhassan42000@gmail.com>
2023-01-25 00:56:40 +0500

    created main view

--- app/src/main/java/shahbaz4311/darul_uloom/MainActivity.java
+++ app/src/main/java/shahbaz4311/darul_uloom/MainActivity.java
@@ -4,11 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.Objects;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //remove the title bar
+        Objects.requireNonNull(getSupportActionBar()).hide();
+
+
     }
 }
